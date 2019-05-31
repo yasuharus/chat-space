8b12efe83d1fc3a92a721e09cc43a349dd3f42ce
yasuharus <harukuroa@icloud.com>
2019-05-31 17:59:50 +0900

    chat-space message2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def edit
   end
 
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
     end
 
   private
