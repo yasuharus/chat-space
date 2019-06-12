11f0d52d6479f837824f92f66395d1eea6463a89
yasuharus <harukuroa@icloud.com>
2019-06-12 19:14:45 +0900

    ajax2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
+  function buildHTML(message){
+    var html = `<div class="message">
+    <div class="message__upper-info">
+    <p class="message__upper-info__talker">
+      ${ message.name}
+    </p>
+    <p class="message__upper-info__date">
+      ${ message.created_at}
+    </p>
+    </div>
+    <p class="message__text">
+    <p class="lower-message__content">
+      ${ message.content}
+    </p>
+    </p>
+    
+    </div>`
+  }
+
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +29,17 @@ $(function() {
       dataType: "json",
       processData: false,
       contentType: false
-    });
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.input-box__text').val('')
+    })
+    .fail(function() {
+      alert('error');
+    })
+    .always(function() {
+      $(".submit-btn").removeAttr("disabled");
+    });   
   });
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,9 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
+    
       respond_to do |format|
-        format.html  {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
+        # format.html  {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
         format.json
       end
     else
