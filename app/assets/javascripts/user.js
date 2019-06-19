a643283b0933a8a93cb26453a55b4b03ed817ea0
yasuharus <harukuroa@icloud.com>
2019-06-19 12:18:58 +0900

    chat-space incremental2

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,10 +1,10 @@
-$(function() {
+$(document).on("turbolinks:load", function() {
   var list = $("#user-search-result");
   
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${ user.name }>追加</div>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${ user.name }">追加</div>
                 </div>`
     list.append(html)
   }
@@ -16,21 +16,50 @@ $(function() {
     list.append(html)
   }
 
+  function appendChatMember(memberId, memberName ) {
+    var member = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'">
+                    <input name='group[user_ids][]' type='hidden' value= "${ memberId } class="aaa"   >
+                    <p class='chat-group-user__name'>${ memberName }</p>
+                    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                  </div>`
 
+  $("#chat-group-users").append(member) 
+   };
+
+  
   $("#user-search-field").on("keyup", function(e) {
     e.preventDefault();
     var input = $(this).val();
- 
+    var group_users = []; 
+    
+    
+    
+    // $(".aaa").each(function() {
+    //   var users = $(this).attr("value");
+    //   group_users.push(users)  
+    // });
+    
+  
+    $(".aaa").each(function() {
+      group_users.push($(this).attr("value"));
+    });
+        
     $.ajax({
       type: "GET",
       url: "/users",
-      data: { keyword: input },
-      dataType: "json"
+      data: { keyword: input,
+              group_users: group_users
+             },
+      dataType: "json",
+      cache: false
     })
     
     .done(function(users) {
       if (users.length !== 0 && input.length !== 0) {
         list.empty();
+
+         
+
         users.forEach(function(user){
           appendUser(user);  
         });
@@ -45,6 +74,21 @@ $(function() {
     .fail(function() {
       alert("ユーザー検索に失敗しました");
     })
-    
   });
+
+  $(document).on("click",".user-search-add ", function() {
+    
+    var memberId = $(this).attr("data-user-id");
+    var memberName = $(this).attr("data-user-name")
+   
+    
+    
+     appendChatMember(memberId, memberName)
+   
+    $(this).parent().remove(); 
+  })
+
+  $(document).on("click",".user-search-remove", function () {
+    $(this).parent().remove(); 
+  }) 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?) and id != ?', "#{params[:keyword]}%", current_user)
+    @users = User.where('name LIKE(?)', "#{params[:keyword]}%").where.not(id:member_users) 
+    
     respond_to do |format|
       format.html
       format.json
@@ -18,9 +19,32 @@ class UsersController < ApplicationController
     end
     end
 
+    def member_users
+      member_users = []
+      member_users << current_user.id
+      if params[:group_users]
+        params[:group_users].map do |user_id|
+        member_users << user_id.to_i
+        
+      end
+    
+    end
+
+    return member_users
+    end
+
+
   private
 
   def user_params
     params.require(:user).permit(:name, :emall)
   end
+
+  
 end
+
+
+
+
+# @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+# @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id:current_user.id)
